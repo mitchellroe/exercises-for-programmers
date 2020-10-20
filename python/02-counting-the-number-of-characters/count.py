7177804ea9cea05f495de8b3a91a4aec25a0bb9e
Mitchell Roe <mbroe@oakland.edu>
2020-10-20 05:58:54 -0400

    Finished exercise 2

---
+++ python/02-counting-the-number-of-characters/count.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Prompt for an input string and print the number of characters
+"""
+
+def main():
+    """
+    Prompt for an input string and print the number of characters
+    """
+    my_string = input("What is the input string? ")
+    num_of_chars = len(my_string)
+    print(my_string + " has " + str(num_of_chars) + " characters.")
+
+if __name__ == '__main__':
+    main()
