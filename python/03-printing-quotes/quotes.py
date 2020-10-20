6fee312260df6f9f46aaa52d67f9ee7a36eb1898
Mitchell Roe <mbroe@oakland.edu>
2020-10-20 06:04:13 -0400

    Finished exercise 3

---
+++ python/03-printing-quotes/quotes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Print out a quote and an author, with quotation marks around the quote.
+"""
+
+def main():
+    """
+    Print out a quote and an author, with quotation marks around the quote.
+    """
+    quote = input("What is the quote? ")
+    author = input("Who said it? ")
+    print(author + " says, \"" + quote + "\"")
+
+if __name__ == '__main__':
+    main()
