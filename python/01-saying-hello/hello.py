f3e47d9b1c37a5e06e670d9a55b689770ecc4b1a
Mitchell Roe <mbroe@oakland.edu>
2020-10-20 05:49:39 -0400

    Starting with Python instead

---
+++ python/01-saying-hello/hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Ask the user for their name and print a greeting.
+"""
+
+def main():
+    """
+    Ask the user for their name and print a greeting.
+    """
+    name = input("What is your name? ")
+    output = "Hello, " + name + ", nice to meet you."
+    print(output)
+
+if __name__ == '__main__':
+    main()
