d272bf53bfb1e2d380b7987635e37b3111fef331
Mitchell Roe <mitchell.roe@erasys.de>
2022-02-01 21:22:04 +0100

    formatting only

--- python/04-mad-lib/mad_lib.py
+++ python/04-mad-lib/mad_lib.py
@@ -3,15 +3,18 @@
 Play a Mad Lib
 """
 
+
 def main():
     """
     Play a Mad Lib
     """
     noun = input("Enter a noun: ")
     verb = input("Enter a verb: ")
-    adj  = input("Enter an adjective: ")
-    adv  = input("Enter an adverb: ")
-    print("Do you " + verb + " your " + adj + " " + noun + " " + adv + "? That's hilarious!")
+    adj = input("Enter an adjective: ")
+    adv = input("Enter an adverb: ")
+    print("Do you " + verb + " your " + adj + " " + noun + " " + adv +
+          "? That's hilarious!")
+
 
 if __name__ == '__main__':
     main()
