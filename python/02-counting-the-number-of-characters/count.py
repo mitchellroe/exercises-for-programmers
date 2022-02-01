ad7cd9bb5bade874697f3ca9904471217950c104
Mitchell Roe <mitchell.roe@erasys.de>
2022-02-01 21:12:00 +0100

    formatting only

--- python/02-counting-the-number-of-characters/count.py
+++ python/02-counting-the-number-of-characters/count.py
@@ -3,6 +3,7 @@
 Prompt for an input string and print the number of characters
 """
 
+
 def main():
     """
     Prompt for an input string and print the number of characters
@@ -10,6 +11,7 @@ def main():
     my_string = input("What is the input string? ")
     num_of_chars = len(my_string)
     print(my_string + " has " + str(num_of_chars) + " characters.")
+
 
 if __name__ == '__main__':
     main()
